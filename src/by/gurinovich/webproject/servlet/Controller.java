f12ee79bf91ef05722fc22274142f21fe179ebe5
MaximHurinovich <maxim.hurinovich@gmail.com>
2018-01-18 19:46:44 +0300

    Initial Commit

--- src/by/gurinovich/webproject/servlet/Controller.java
+++ src/by/gurinovich/webproject/servlet/Controller.java
@@ -26,7 +26,7 @@ public class Controller extends HttpServlet {
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String page = null;
 
-        ActionFactory client = new ActionFactory();
+        ActionFactory client = new ActionFactory()  ;
         ActionCommand command = client.defineCommand(request);
         page = command.execute(request);
         if (page != null) {
